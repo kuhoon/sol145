afa0fa3f239ab173d6ac911143285a7e7075e079
kuhoon <101597750+kuhoon@users.noreply.github.com>
2022-03-19 00:05:10 +0100

    start

--- writeFileSample4.py
+++ writeFileSample4.py
@@ -1,1 +1,13 @@
+# ctrl+d = 해당 줄 복사, ctrl+/ = 주석처리, shift + d / = 여러줄, ctrl shift alt j = 전체변환
 
+import os
+
+# from IPython.display import HTML as html_print
+from pyNastran.bdf.bdf import BDF, CaseControlDeck
+model = BDF()
+
+bdf_filename_out = os.path.join('sol145_OUT.bdf')
+model.write_bdf(bdf_filename_out, enddata=True)
+print(bdf_filename_out)
+
+print('----------------------------------------------------------------------------------------------------')
